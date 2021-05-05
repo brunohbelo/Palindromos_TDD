0c43462e699077f0a59f66553395d8be06d2a326
Bruno Belo <brunohbelo@hotmail.com>
2021-05-05 13:10:46 -0300

    Add project files.

---
+++ Palindromos_TDD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Palindromos_TDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
