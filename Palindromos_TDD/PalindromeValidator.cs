05c7f8afe8b33317fcc4c62621acf4db736e9015
Bruno Belo <brunohbelo@hotmail.com>
2021-05-05 13:51:43 -0300

    project concluded

---
+++ Palindromos_TDD/PalindromeValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Palindromos_TDD
+{
+    public static class PalindromeValidator
+    {
+        public static bool IsPalindrome(string palindrome)
+        {
+            if (string.IsNullOrEmpty(palindrome) || string.IsNullOrWhiteSpace(palindrome))
+                return false;
+
+            char[] letters = palindrome.ToCharArray();
+            Array.Reverse(letters);
+            var reversedPalindrome = new String(letters);
+
+            var result = palindrome == reversedPalindrome;
+
+            return result;
+        }
+    }
+}
---
+++ Palindromos_TDD/Tests/PalindromeValidatorTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Palindromos_TDD.Tests
+{
+    public static class PalindromeValidatorTest
+    {
+
+        [Fact]
+        public static void IsPalindromeShouldNotAcceptNull()
+        {
+            //Arrange
+            string palindrome = null;
+
+            //Act
+            var wordIsPalindrome = PalindromeValidator.IsPalindrome(palindrome);
+
+            //Assert
+            Assert.False(wordIsPalindrome);
+        }
+
+        [Fact]
+        public static void IsPalindromeShouldNotAcceptOnlySpaces()
+        {
+            //Arrange
+            string palindrome = "   ";
+
+            //Act
+            var wordIsPalindrome = PalindromeValidator.IsPalindrome(palindrome);
+
+            //Assert
+            Assert.False(wordIsPalindrome);
+        }
+
+        [Fact]
+        public static void IsPalindromeShouldBeFalse()
+        {
+            //Arrange
+            string palindrome = "xyz";
+
+            //Act
+            var wordIsPalindrome = PalindromeValidator.IsPalindrome(palindrome);
+
+            //Assert
+            Assert.False(wordIsPalindrome);
+        }
+
+        [Fact]
+        public static void IsPalindromeShouldBeTrue()
+        {
+            //Arrange
+            string palindrome = "madam";
+
+            //Act
+            var wordIsPalindrome = PalindromeValidator.IsPalindrome(palindrome);
+
+            //Assert
+            Assert.True(wordIsPalindrome);
+        }
+
+
+    }
+}
